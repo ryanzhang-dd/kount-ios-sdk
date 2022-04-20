9d52a771225a60b0998e32d9cbdb4cf8285bbcd6
Ryan Zhang <ryan.zhang@doordash.com>
2022-04-20 14:02:47 -0700

    commit package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KountDataCollect",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "KountDataCollect", targets: ["KountDataCollect"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "KountDataCollect", url: "https://github.com/ryanzhang/kount-ios-sdk/releases/download/4.1.5-doordash/KountDataCollect.xcframework.zip", checksum: "be7379822ea8ccf19892d5c8d05495ce8f15d1e1ed6e684ce649645aaaa55c90")
+    ]
+)
